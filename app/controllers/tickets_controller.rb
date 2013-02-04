bf891418bf90d59cadb1ae376151c7fd75cdfb3c
cherimarie <cherimarie@gmail.com>
2013-02-04 10:36:43 -0800

    implemented edit action for the tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -17,16 +17,29 @@ before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
 	  end
 	end
 
-	private
-	def find_project
-		@project = Project.find(params[:project_id])
-	end
-
 	def find_ticket
 		@ticket = @project.tickets.find(params[:id])
 	end
 
 	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @ticket.update_attributes(params[:ticket])
+			flash[:notice] = "Ticket has been updated."
+			redirect_to [@project, @ticket]
+		else
+			flash[:alert] = "Ticket has not been updated."
+			render :action => "edit"
+		end
+	end
+
+	private
+	def find_project
+		@project = Project.find(params[:project_id])
 	end
 
 end

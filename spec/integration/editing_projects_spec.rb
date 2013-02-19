fa20796f1418053cfee9df76e3e51b59efa13f6d
cherimarie <cherimarie@gmail.com>
2013-02-18 20:46:27 -0800

    restrict access to project actions to admins only

--- spec/integration/editing_projects_spec.rb
+++ spec/integration/editing_projects_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 feature "Editing Projects" do
 
 	before do
+		sign_in_as!(Factory(:admin_user))
 		Factory(:project, :name => "Turtle Wars")
 	    visit "/"
 	    click_link "Turtle Wars"

e3726d1a11f2f38000930991b925705a92f28c67
cherimarie <cherimarie@gmail.com>
2013-02-24 18:09:08 -0800

    restrict creating tickets to only users who have permission to do so

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -5,6 +5,7 @@ describe TicketsController do
   let(:ticket) { Factory(:ticket, :project => project,
                                 :user => user) }
 	context "standard users" do
+
 	  it "cannot access a ticket for a project" do
 	    sign_in(:user, user)
 	    get :show, :id => ticket.id, :project_id => project.id
@@ -12,5 +13,26 @@ describe TicketsController do
 	    flash[:alert].should eql("The project you were looking " +
 	"for could not be found.")
 	  end
+
+	  context "with permission to view the project" do
+		  before do
+		    sign_in(:user, user)
+		    define_permission!(user, "view", project)
+		  end
+		  def cannot_create_tickets!
+		    response.should redirect_to(project)
+		    message = "You cannot create tickets on this project."
+		    flash[:alert].should eql(message)
+		  end
+		  it "cannot begin to create a ticket" do
+		    get :new, :project_id => project.id
+		    cannot_create_tickets!
+		  end
+		  it "cannot create a ticket without permission" do
+		    post :create, :project_id => project.id
+		    cannot_create_tickets!
+		  end
+	  end
+
     end
 end
